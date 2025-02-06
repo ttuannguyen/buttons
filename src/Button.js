d0d25a0fcc999cf56ec25e9f1ef80a11ca844a4f
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-05 20:51:48 -0500

    add more styling to buttons

--- src/Button.js
+++ src/Button.js
@@ -15,7 +15,7 @@ const Button = ({
   //     throw new Error('only one should be provided');
   //   }
 
-  return <button className="bg-red-500">{children}</button>;
+  return <button className="px-3 py-1.5 border border-blue-700 bg-blue-500 text-white">{children}</button>;
 };
 
 Button.propTypes = {
