6e2f9b4fedd4dd91f044ab4e86e1d3dec4b88265
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-05 19:52:00 -0500

    install PropTypes

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const Button = ({ children }) => {
-  return (
+const Button = ({ 
+    children, 
+    primary, 
+    secondary, 
+    success, 
+    warning, 
+    danger,
+    outline, 
+    rounded
+ }) => {
+  
+//   if (primary && secondary) {
+//     throw new Error('only one should be provided');
+//   }
+  
+    return (
     <button>{children}</button>
   )
 }
