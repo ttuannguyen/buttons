f9c4dd2b2baa1964b3e8df92ac81e35952e2d9a9
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-05 19:41:03 -0500

    add text to each button in App and pass down to Button as children prop

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
       <div>
-        <div><Button /></div>
-        <div><Button /></div>
-        <div><Button /></div>
-        <div><Button /></div>
+        <div><Button>Click me!!</Button></div>
+        <div><Button>Buy now!!</Button></div>
+        <div><Button>See deal!!</Button></div>
+        <div><Button>Hide ad!!</Button></div>
       </div>
     </>
   );
--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ children }) => {
   return (
-    <button>Hi there!</button>
+    <button>{children}</button>
   )
 }
 
