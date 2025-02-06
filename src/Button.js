fa783fbe9bf01b2e866e707b2bdaedca20db6fcf
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-05 20:08:32 -0500

    add validation with PropTypes

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ 
-    children, 
-    primary, 
-    secondary, 
-    success, 
-    warning, 
-    danger,
-    outline, 
-    rounded
- }) => {
-  
-//   if (primary && secondary) {
-//     throw new Error('only one should be provided');
-//   }
-  
-    return (
-    <button>{children}</button>
-  )
-}
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
+  //   if (primary && secondary) {
+  //     throw new Error('only one should be provided');
+  //   }
 
-export default Button
+  return <button>{children}</button>;
+};
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!success)
+        + Number(!!warning)
+        + Number(!!danger)
+
+    if (count > 1) {
+        return new Error('Only one of primary, secondary, success, warning, danger can be true');
+    }
+  },
+};
+
+export default Button;
