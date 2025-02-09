24a5cd6ead7193d90978baa870670dd3f03cdbb7
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-09 15:43:13 -0500

    add an options prop containing an array of labels and pass it down to DropDown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import { useState } from "react";
-import { GoBell, GoChecklist, GoBroadcast } from "react-icons/go";
-import Button from "./components/Button";
-import Accordion from "./components/Accordion";
+import DropDown from "./components/DropDown";
 
 function App() {
-  const items = [
-    {
-      id: 1,
-      label: 'Can I use React on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
-    {
-      id: 2,
-      label: 'Can I use Javascript on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
-    {
-      id: 3,
-      label: 'Can I use CSS on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
-  ];
-  
-  return <Accordion items={items} />;
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' }
+  ]
+  return <DropDown options={options} />;
 }
 
 export default App;
