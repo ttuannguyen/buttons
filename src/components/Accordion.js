71490a140d4a524fd73b102e81ccc455f8e42d05
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-09 13:12:32 -0500

    conditionally render content based of the state of isExpanded

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,68 +1,25 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(0);
 
-    const renderedItems = items.map((item, index) => {
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
 
-        if (index === expandedIndex) {
-            console.log('expanded');
-        } else {
-            console.log('collapsed');
-        }
+    if (index === expandedIndex) {
+      console.log("expanded");
+    } else {
+      console.log("collapsed");
+    }
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
 
   return <div>{renderedItems}</div>;
-
-//   const handleClick = () => {
-//     console.log("click!");
-//   };
-
-//   return (
-//     <>
-//       <div>
-//         <div>
-//           <Button
-//             success
-//             rounded
-//             outline
-//             className="mb-5"
-//             onClick={handleClick}
-//           >
-//             <GoBell />
-//             Click me!!
-//           </Button>
-//         </div>
-//         <div>
-//           <Button danger outline>
-//             <GoChecklist />
-//             Buy now!!
-//           </Button>
-//         </div>
-//         <div>
-//           <Button warning>See deal!!</Button>
-//         </div>
-//         <div>
-//           <Button secondary outline>
-//             <GoBroadcast />
-//             Hide ad!!
-//           </Button>
-//         </div>
-//         <div>
-//           <Button primary rounded>
-//             Something!
-//           </Button>
-//         </div>
-//       </div>
-//     </>
-//   );
-
 };
 
 export default Accordion;
