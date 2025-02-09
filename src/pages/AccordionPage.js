fd69aa57ab32bc90faa3b3251ab48e2c79e64ac3
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-09 15:38:22 -0500

    add an Accordion page

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Accordion from '../components/Accordion';
+
+const AccordionPage = () => {
+    const items = [
+        {
+          id: 1,
+          label: 'Can I use React on a project?',
+          content:
+            'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        },
+        {
+          id: 2,
+          label: 'Can I use Javascript on a project?',
+          content:
+            'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        },
+        {
+          id: 3,
+          label: 'Can I use CSS on a project?',
+          content:
+            'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+        },
+      ];
+      
+      return <Accordion items={items} />;
+}
+
+export default AccordionPage;
