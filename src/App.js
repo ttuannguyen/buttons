718b90afa9aeaa4de08b9bb8ad704cb609038d49
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-15 12:42:24 -0500

    implement controlled form for selecting option from dropdown

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import DropDown from "./components/DropDown";
 
 function App() {
   const [selection, setSelection] = useState(null);
-  
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' }
-  ]
-  return <DropDown options={options} />;
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+  return (
+    <DropDown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 
 export default App;
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
 
-const DropDown = ({ options }) => {
+const DropDown = ({ options, selection, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionClick = (event) => {
+  const handleOptionClick = (option) => {
     // Close dropdown
     setIsOpen(false);
     // Option the user clicks on
-    console.log(event);
-  }
-  
+    onSelect(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div onClick={() => handleOptionClick} key={option.value}>{option.label}</div>;
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
   });
+
+  let content = "Select...";
+  if (selection) {
+    content = selection.label;
+  }
 
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
+      <div onClick={handleClick}>{content}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
