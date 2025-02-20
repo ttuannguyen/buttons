81c97e8e624ce9d15c4c49b19437f63bb3f4c2b5
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 00:27:53 -0500

    add a navigation function and use pushState to send the user to a particular path

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,9 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+        setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', handler);
+
+    return () => {
+        window.removeEventListener('popstate');
+    }
+  }, []);
+
+  const navigate = (to) => {
+    window.history.pushState({}, '', to);
+    setCurrentPath(to);
+  }
 
   return (
     <NavigationContext.Provider value={{}}>
