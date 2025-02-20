61366bea58e321368f6db4981fc92c05b6c438ee
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 00:12:15 -0500

    add currentPath state to keep track of the the path

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
