42429703e2edb70a108c1c594168898a34def8dd
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 22:25:35 -0500

    add an array with label and path keys in Sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,1 +1,15 @@
 import Link from "./Link";
+
+const Sidebar = () => {
+    const links = [
+        { label: 'Dropdown', path: '/' }
+    ];
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Sidebar;
