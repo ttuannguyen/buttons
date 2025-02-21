42ab3ef935cf6f3b1de09434a4c448b1a32d660e
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 19:24:50 -0500

    leverage Route component and add conditional logic in App to route the user to the appropriate page based on the current path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
-
   return (
     <div>
       <Link to="/accordion">Go to Accordion</Link>
       <Link to="/dropdown">Go to DropDown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NavigationContext from '../context/navigation';
 
-const Route = () => {
-  return (
-    <div>Route</div>
-  )
+const Route = ({ path, children }) => {
+    const { currentPath } = useContext(NavigationContext);
+  
+    if (path === currentPath) {
+        return children;
+    }
+    
+    return null;
 }
 
-export default Route
+export default Route;
