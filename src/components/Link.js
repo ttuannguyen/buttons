7eb585c0aff281df9ea7ad5288026b9df57f6898
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 22:02:10 -0500

    add a conditional to let the browser open new page

--- src/components/Link.js
+++ src/components/Link.js
@@ -7,10 +7,13 @@ const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
   
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+    
     event.preventDefault();
 
     navigate(to);
-
   };
   return <a onClick={handleClick}>{children}</a>;
   
