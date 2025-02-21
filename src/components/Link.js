13aa00e98d147e0ecbe4baa542ba7042d33e6766
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-20 22:07:36 -0500

    add styling to links

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,23 +1,27 @@
+import classNames from "classnames";
 import { useContext } from "react";
-
 import React from "react";
 import NavigationContext from "../context/navigation";
 
 const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
-  
+
+  const classes = classNames("text-blue-500");
+
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
       return;
     }
-    
+
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
-  
-  
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
