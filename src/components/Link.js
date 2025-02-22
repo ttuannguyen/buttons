6d11771b48c312303323c6a297bf8eb7744a23bb
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-22 17:24:12 -0500

    highlight the Active Link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,14 @@
 import classNames from "classnames";
 import useNavigation from "../hooks/use-navigation";
 
-const Link = ({ to, children, className }) => {
-  const { navigate } = useNavigation();
+const Link = ({ to, children, className, activeClassName }) => {
+  const { navigate, currentPath } = useNavigation();
 
-  const classes = classNames("text-blue-500", className);
+  const classes = classNames(
+    "text-blue-500", 
+    className,
+    currentPath === to && activeClassName // i.e. we're going to end up with activeClassName if currentPath is equal to true
+  );
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
