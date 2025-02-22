7323608cc1bfde0a3a6f9e121f629302123cc568
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-02-22 17:07:34 -0500

    add styling so that only the links in the sidebar have margin at the bottom

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
 
   const renderedLinks = links.map((link) => {
     return (
-      <Link key={link.label} to={link.path}>
+      <Link key={link.label} to={link.path} className="mb-3">
         {link.label}
       </Link>
     );
